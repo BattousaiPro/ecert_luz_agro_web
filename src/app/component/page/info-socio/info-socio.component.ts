c1119b102be7c08ffa6b19d51187c6b630653a52
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-04-14 19:22:49 -0400

    avance

--- src/app/component/page/info-socio/info-socio.component.ts
+++ src/app/component/page/info-socio/info-socio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { listaSocios } from './model/dataMock';
 
 @Component({
   selector: 'app-info-socio',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './info-socio.component.scss'
 })
 export class InfoSocioComponent {
+  socios: any[] = listaSocios;
 
+  cambioEstado(index: any) {
+    listaSocios[index].selected = !listaSocios[index].selected;
+  }
 }
