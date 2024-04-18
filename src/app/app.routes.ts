29c1b9cb8ce0156dc222f328eb548acb35caa0fb
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-04-18 00:23:23 -0400

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { InfoSocioComponent } from './component/page/info-socio/info-socio.component';
+import { PermisosComponent } from './component/permisos/permisos.component';
+import { RolesComponent } from './component/roles/roles.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'infosocio', component: InfoSocioComponent },
+    { path: 'roles', component: RolesComponent },
+    { path: 'permisos', component: PermisosComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
