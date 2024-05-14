8ee4aebc4625fdfaae5c5da433d5cb5f4a6d7e1a
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-05-14 04:20:24 -0400

    fix

--- src/app/services/comunas/comunas.service.ts
+++ src/app/services/comunas/comunas.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComunasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public obtenerComunById(id: number): Observable<any> {
+    let url = 'http://localhost:3000/comunas/' + id;
+    const ladata: Observable<any> = this.http.get(
+      url
+    );
+    return ladata;
+  }
+
 }
--- src/app/services/sector/sector.service.ts
+++ src/app/services/sector/sector.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SectorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public obtenerSectorById(id: number): Observable<any> {
+    let url = 'http://localhost:3000/sector';
+    const ladata: Observable<any> = this.http.get(
+      url
+    );
+    return ladata;
+  }
+
 }
