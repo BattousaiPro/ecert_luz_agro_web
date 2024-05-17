83f8dac7ea4421bb43e586f97991f7d7718f8285
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-05-17 19:24:58 -0400

    aaaa

--- src/app/component/page/detail-socio/detail-socio.component.ts
+++ src/app/component/page/detail-socio/detail-socio.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { DataSocio } from '../info-socio/model/dataMock';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-socio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-socio.component.html',
   styleUrl: './detail-socio.component.scss'
 })
