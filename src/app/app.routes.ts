995a3389cda788fb4c0036370d334cd8ac292e54
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-05-18 15:51:50 -0400

    add permisos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { UsuariosComponent } from './component/usuarios/usuarios.component';
 import { RolesComponent } from './component/roles/roles.component';
 import { FichasSociosComponent } from './component/page/fichas-socios/fichas-socios.component';
 import { HomeComponent } from './component/home/home.component';
+import { PermisosComponent } from './component/permisos/permisos.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'infosocio', component: InfoSocioComponent },
     { path: 'roles', component: RolesComponent },
+    { path: 'permisos', component: PermisosComponent },
     { path: 'usuarios', component: UsuariosComponent },
     { path: 'fichas/socios', component: FichasSociosComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' },
