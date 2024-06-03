cc749f9a10e299a7b3d724510cbe802ee603fa40
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-06-03 03:27:23 -0400

    sdasdas

--- src/app/services/rol-permiso/rol-permiso.service.ts
+++ src/app/services/rol-permiso/rol-permiso.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolPermisoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public setPermisoToRol(idRol: number, listIdPermisos: number[]): Observable<any> {
+    let url = 'http://localhost:3000/rol-permiso/' + idRol;
+    const ladata: Observable<any> = this.http.post(
+      url,
+      listIdPermisos
+    );
+    return ladata;
+  }
+
 }
--- src/app/services/user-rol/user-rol.service.ts
+++ src/app/services/user-rol/user-rol.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRolService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public setRolToUser(idUser: number, listIdRoles: number[]): Observable<any> {
+    let url = 'http://localhost:3000/user-rol/' + idUser;
+    const ladata: Observable<any> = this.http.post(
+      url,
+      listIdRoles
+    );
+    return ladata;
+  }
+
 }
